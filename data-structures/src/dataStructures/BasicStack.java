3873a0a9793705d6b1215786eadeb7dcd70c9440
Desmond Beramendi <desmond.bera@gmail.com>
2019-09-08 13:13:10 -0400

    Card Stack App implemented to test BasicStack

--- data-structures/src/dataStructures/BasicStack.java
+++ data-structures/src/dataStructures/BasicStack.java
@@ -1,68 +1,70 @@
+package dataStructures;
 
 public class BasicStack<X> {
-	
+
 	// Using a primitive data type of array w/ Generic type
-	private X [] data;
-	// An internal pointer on our data structure - not available thru getters / setters
+	private X[] data;
+	// An internal pointer on our data structure - not available thru getters /
+	// setters
 	private int stackPointer;
-	
-	//Constructor
+
+	// Constructor
 	public BasicStack() {
 		this.data = (X[]) new Object[1000];
 		stackPointer = 0;
 	}
-	
+
 	// Create insert / delete methods (push / pop)
 	// Time complexity: O(1)
 	public void push(X newItem) {
-		//add to underlying array 
-		//also uses post-increment
+		// add to underlying array
+		// also uses post-increment
 		data[stackPointer++] = newItem;
 	}
-	
+
 	// Create contains / access methods (search)
 	// Time complexity: O(n)
 	public X pop() {
-		
-		//Handle exception when stack is empty
-		if(stackPointer == 0) {
+
+		// Handle exception when stack is empty
+		if (stackPointer == 0) {
 			throw new IllegalStateException("No more items on the stack.");
 		}
-		
-		//uses pre-increment
+
+		// uses pre-increment
 		return data[--stackPointer];
 	}
-	
+
 	// Search data structure for x-item
 	// Time complexity: O(n)
 	public boolean contains(X item) {
 		boolean found = false;
-		
-		for(int x = 0; x < stackPointer; x++) {
-			if(data[x].equals(item)) {
+
+		for (int x = 0; x < stackPointer; x++) {
+			if (data[x].equals(item)) {
 				found = true;
 				break;
 			}
 		}
-		
+
 		return found;
 	}
-	
+
 	// Retrieve x-item
 	// Time complexity: O(n)
 	public X access(X item) {
-		while(stackPointer > 0) {
+		while (stackPointer > 0) {
 			X tempItem = pop();
-			if(item.equals(tempItem)) {
+			if (item.equals(tempItem)) {
 				return tempItem;
 			}
 		}
 		throw new IllegalArgumentException("Could not find itme onthe stack: " + item);
 	}
-	
-	//Size of our data structure
+
+	// Size of our data structure
 	public int size() {
 		return stackPointer;
 	}
-	
+
 }
