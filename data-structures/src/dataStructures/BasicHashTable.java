afba87cef9a2810526869f7aaf93b4ef7905a3c1
Desmond Beramendi <desmond.bera@gmail.com>
2019-09-10 10:23:48 -0400

    Added delete + hasKey + hasValue methods for hash table.

--- data-structures/src/dataStructures/BasicHashTable.java
+++ data-structures/src/dataStructures/BasicHashTable.java
@@ -13,6 +13,60 @@ public class BasicHashTable<X, Y> {
 	}
 	
 	
+	//This delete implementation can be problematic if we hit the capacity of our hash table
+	public Y delete(X key) {
+		// first get the value for the key and return the removed value
+		Y value = get(key);
+		
+		//second, clear out the hash table slot for the key and return the removed value
+		if(value != null) {
+			int hash = calculateHash(key);
+			data[hash] = null;
+			size--;
+			hash = (hash + 1) % this.capacity;
+			
+			//if the next slot isn't empty we should re-add it so we can keep the hash algorithms clean
+			while(data[hash] != null) {
+				HashEntry he = data[hash];
+				data[hash] = null;
+				put((X)he.getKey(), (Y)he.getValue());
+				
+				//we repositioned the hash item and didn't really add a new one so back off the size
+				size--;
+				hash = (hash + 1) % this.capacity;
+			}
+		}
+		return value;
+	}
+	
+	public boolean hasKey(X key) {
+		int hash = calculateHash(key);
+		
+		//if we don't have anything for the given key, we can just return false
+		if(data[hash] != null) {
+			return false;
+		} else {
+			// otherwise get the hashentry for the key and see if it matches the given key
+			if(data[hash].getKey().equals(key)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean hasValue(Y value) {
+		// loop through all of the hash entries
+		for(int x = 0; x < this.capacity; x++) {
+			HashEntry entry = data[x];
+			
+			//if this hash entry isn't null and the value equals the one passed in, the hashtable has this value
+			if(entry != null && entry.getValue().equals(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public Y get(X key) {
 		int hash = calculateHash(key);
 		
