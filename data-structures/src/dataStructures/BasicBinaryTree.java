dfcb334963dea17a86bff746aff7313cfb9cdb89
Desmond Beramendi <desmond.bera@gmail.com>
2019-09-10 17:50:11 -0400

    Added insert + add methods for binary tree

--- data-structures/src/dataStructures/BasicBinaryTree.java
+++ data-structures/src/dataStructures/BasicBinaryTree.java
@@ -3,9 +3,59 @@ package dataStructures;
 public class BasicBinaryTree<X extends Comparable<X>> {
 
 	private Node root;
+	private int size;
+	
 	
 	public BasicBinaryTree() {
 		this.root = null;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void add(X item) {
+		Node node =  new Node(item);
+		
+		//if this is the first item in the tree, set it as root
+		if(root == null) {
+			this.root = node;
+			System.out.println("Set root: " + node.getItem());
+			this.size++;
+		} else {
+			insert(this.root, node);
+		}
+		
+	}
+	
+	//Helper method to recurse when we use the ADD method
+	private void insert(Node parent, Node child) {
+		//if the child is less than parent, it goes on the left
+		if(child.getItem().compareTo(parent.getItem()) < 0) {
+			//if the left node is null, we've found our spot
+			if(parent.getLeft() == null) {
+				parent.setLeft(child);
+				child.setParent(parent);
+				this.size++;
+			} else {
+				//otherwise we need to call insert again and test for left or right (recursioooon)
+				insert(parent.getLeft(), child);
+			}
+		} else if(child.getItem().compareTo(parent.getItem()) > 0) {
+			//if the right node is null, we've found our spot
+			if(parent.getRight() == null) {
+				parent.setRight(child);
+				child.setParent(parent);
+				this.size++;
+			} else {
+				//again, we need to recurse test for the left or right
+				insert(parent.getRight(), child);
+			}
+		}
+		
+		//if the parent and the child heppen to be equal, we won't do anything
+		// data in a binary tree is assumed to be unique and the value alreadys exists in the tree
+		// if you want to allow duplicates we would have some more code here
 	}
 	
 	private class Node {
