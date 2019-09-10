0f7a5a7c948c973ba37a21f054cf5896ed660073
Desmond Beramendi <desmond.bera@gmail.com>
2019-09-09 21:43:50 -0400

    Basic structure for hash tabel.

---
+++ data-structures/src/dataStructures/BasicHashTable.java
@@ -0,0 +1,38 @@
+package dataStructures;
+
+public class BasicHashTable<X, Y> {
+
+	private HashEntry[] data;
+	private int capacity;
+	
+	public BasicHashTable(int tableSize) {
+		this.capacity = tableSize;
+		this.data = new HashEntry[this.capacity];
+	}
+	
+	private class HashEntry<X, Y> {
+		private X key;
+		private Y value;
+		
+		public HashEntry(X key, Y value){
+			this.key = key;
+			this.value = value;
+		}
+		
+		public X getKey() {
+			return key;
+		}
+		public void setKey(X key) {
+			this.key = key;
+		}
+		public Y getValue() {
+			return value;
+		}
+		public void setValue(Y value) {
+			this.value = value;
+		}
+		
+		
+	}
+	
+}
